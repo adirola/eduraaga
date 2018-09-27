c6d4e31ad3f4627146784447cf226cbc9a2c73e3
Aditya  Rola <adityarola@Adityas-MacBook-Air.local>
2018-09-27 22:50:42 +0530

    final

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,32 @@
   
 
 */
+
+            function objectifyForm(formArray) {//serialize data function
+
+                var returnArray = {};
+                for (var i = 0; i < formArray.length; i++){
+                returnArray[formArray[i]['name']] = formArray[i]['value'];
+                }
+                return returnArray;
+
+                
+            };
+       
+            function submitform(form){
+                form1 = document.getElementById('myForm');
+                var data1 = objectifyForm(form1);
+                $.ajax({
+                    url: "http://localhost:9000/api/smartHome",
+                    type: "POST",
+                    data: data1,
+                    success : function (result){
+                        alert(result);
+                    },
+                    error: function(){
+                        console.log('error happened');
+                    }
+                })
+                
+                
+            };
